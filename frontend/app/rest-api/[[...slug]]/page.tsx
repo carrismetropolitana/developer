1369b9e2abacc0568eed0f3679346518e681fe53
João de Vasconcelos <contact@joao.earth>
2024-12-26 17:40:34 +0000

    Add new API documentation for metrics, locations, stops, network, facilities, and types

--- frontend/app/rest-api/[[...slug]]/page.tsx
+++ frontend/app/rest-api/[[...slug]]/page.tsx
@@ -1,6 +1,8 @@
 /* * */
 
 import { restApiDocs } from '@/lib/source';
+import { Banner } from 'fumadocs-ui/components/banner';
+import { TypeTable } from 'fumadocs-ui/components/type-table';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { DocsBody, DocsDescription, DocsPage, DocsTitle } from 'fumadocs-ui/page';
 import { notFound } from 'next/navigation';
@@ -19,7 +21,7 @@ export default async function Page(props: { params: Promise<{ slug?: string[] }>
 			<DocsTitle>{page.data.title}</DocsTitle>
 			<DocsDescription>{page.data.description}</DocsDescription>
 			<DocsBody>
-				<MDX components={{ ...defaultMdxComponents }} />
+				<MDX components={{ ...defaultMdxComponents, Banner, TypeTable }} />
 			</DocsBody>
 		</DocsPage>
 	);
